a225042ddc36ef0f01adf89d4fc6d1974147ac34
phoebekaranja <phoebemurish@gmail.com>
2018-10-07 11:20:06 +0300

    created a class test and made some changes

--- app/main/views.py
+++ app/main/views.py
@@ -4,16 +4,35 @@ from app import app
 # Views
 @app.route('/')
 def index():
+    '''
+    my index page
+    :return:
+    '''
+    sources=  get sources()
+    print(Sources)
+    message= "Test Dynamic message"
+    return render_template('index.html' sources =sources, message = message)
+@main.route('/articles/<id>')
+def articles(id):
+    '''
+    View  page function that returns the  details page and its data
+    '''
+    Name = "Phoebe"
+    articles=get_articles(id)
+    return render_template('articles.html', articles = articles, name=name,name_source=id )
+# print(articles)
+    # title=f'{articles.title}'
 
-    '''
-    View root page function that returns the index page and its data
-    '''
-    message = 'Hello World'
-    return render_template('index.html',message = message)
-@app.route('/movie/<movie_id>')
-def movie(movie_id):
 
-    '''
-    View movie page function that returns the movie details page and its data
-    '''
-    return render_template('movie.html',id = movie_id)
+# @main.route('/categories/<category_name>')
+# def categories(category_name):
+#
+#     '''
+#     View  page function that returns the details page and its data
+#     '''
+#     cat="cherucole"
+#     category=get_category(category_name)
+#     print(category_name)
+#     category_name1=category_name
+#
+#     return render_template('categories.html', category=category,name_source=id,category_name1=category_name1)
--- config.py
+++ config.py
@@ -3,8 +3,8 @@ class Config:
     General configuration parent class
     '''
     pass
-    # SOURCE_NEWS_URL='https://newsapi.org/v2/top-headlines?sources={}&apiKey={}'
-    #
+    SOURCE_NEWS_URL='https://newsapi.org/v2/top-headlines?sources={}&apiKey={}'
+    
     # NEWS_API_SOURCE_URL = 'https://newsapi.org/v2/sources?apiKey={}'
     # # CATEGORIES_URL='https://newsapi.org/v2/everything?q={}&sortBy=relevancy&apiKey={}'
     #
